1356fc539893eab1cd4b719ef69c582cf78366df
Chris Schnabel <cschnabel@softrek.com>
2022-01-21 23:24:08 -0500

    Corrected capitalization in import.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Header from '../components/header/header'
+import Header from '../components/header/Header'
 import Highlights from '../components/highlights/Highlights'
 import prisma from '../lib/prisma'
 
