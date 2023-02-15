496b9fac9398676d69d21221e2a5ec89bc6cd5dd
Chris Schnabel <chrisjschnabel@gmail.com>
2023-02-15 18:27:00 -0500

    Added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.js",
+    "./pages/**/*.js",
+    "./components/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
