33ebf31e8d8bcf370a73ee7cff027d691f839d1e
Chris Schnabel <chrisjschnabel@gmail.com>
2023-02-19 18:24:08 -0500

    Search on enter

--- components/filters/Filters.js
+++ components/filters/Filters.js
@@ -31,48 +31,52 @@ function Filters({ books, initialSearchText, initialSelectedBooks, onSearch }) {
     setSearchText(e.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e && e.preventDefault()
+    onSearch(searchText, selectedBooks)
+  }
+
   const _onClear = () => {
     setSearchText('')
     setSelectedBooks([])
     onSearch('', [])
   }
 
-  const _onSearch = () => {
-    onSearch(searchText, selectedBooks)
-  }
 
   return (
     <div>
-      <div className="filter">
-        <Select
-          options={ options }
-          isMulti
-          placeholder="Filter by book"
-          value={ selectedBooks }
-          onChange={ handleBookFilterChange }
-          width="20em"
-        />
-      </div>
-      <div className="filter">
-        <input
-          type="text"
-          placeholder="Search"
-          value={ searchText }
-          onChange={ handleSearchFilterChange }
-          className="block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-        />
-      </div>
-      <div className="flex justify-center pt-2">
-        <button
-          onClick={ _onSearch }
-          className="w-20 bg-gray-900 text-white py-2 px-4 rounded hover:underline"
-          style={{ marginRight: '10px' }}
-        >Search</button>
-        <button
-          onClick={ _onClear }
-          className="w-20 bg-white text-gray-900 py-2 px-4 rounded border border-gray-900 hover:underline"
-        >Clear</button>
-      </div>
+      <form onSubmit={ handleSubmit }>
+        <div className="filter">
+          <Select
+            options={ options }
+            isMulti
+            placeholder="Filter by book"
+            value={ selectedBooks }
+            onChange={ handleBookFilterChange }
+            width="20em"
+          />
+        </div>
+        <div className="filter">
+          <input
+            type="text"
+            placeholder="Search"
+            value={ searchText }
+            onChange={ handleSearchFilterChange }
+            className="block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+          />
+        </div>
+        <div className="flex justify-center pt-2">
+          <button
+            type="submit"
+            className="w-20 bg-gray-900 text-white py-2 px-4 rounded hover:underline"
+            style={{ marginRight: '10px' }}
+          >Search</button>
+          <button
+            onClick={ _onClear }
+            className="w-20 bg-white text-gray-900 py-2 px-4 rounded border border-gray-900 hover:underline"
+          >Clear</button>
+        </div>
+      </form>
     </div>
   )
 }
